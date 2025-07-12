81a2dfaa909f4c7cf0a469654965b4a17f504f1a
Minsu Seo <lullulalal@gmail.com>
2025-07-12 23:57:43 +0200

    Change baseURL to debug

--- lib/constants/api_constants.dart
+++ lib/constants/api_constants.dart
@@ -1,6 +1,6 @@
 class ApiConstants {
-  //static const String baseUrl = 'http://127.0.0.1:8000';
-  static const String baseUrl = 'https://138.199.215.9.nip.io';
+  static const String baseUrl = 'http://127.0.0.1:8000/dokkaebiimage';
+  //static const String baseUrl = 'https://138.199.215.9.nip.io/dokkaebiimage';
 
   static const String colorTransfer = '$baseUrl/colorTransfer';
   static const String noiseRemover = '$baseUrl/noiseRemover';
